b420b2773c2ddbcf02e6dae079b9e86290fa5a9f
Felipe Maia <felipemaia850@gmail.com>
2024-09-02 15:50:30 -0300

    feat: add benefits section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
       <section className={styles.hero}>
         <CardImage
           src={cardImg1}
-          className={`${styles.cardImg} ${styles.cardImgHero}`}
+          className={`${styles.cardImg} ${styles.imgHero}`}
         />
         <h1>Transforme sua vida com quem já esteve no seu lugar</h1>
         <div className={styles.buttonGroup}>
@@ -83,6 +83,88 @@ export default function Home() {
           eu já estive no seu lugar e sei que a <b>mudança é possível</b>.
         </p>
         <CardImage src={cardImg6} className={styles.cardImg} />
+      </section>
+
+      <section className={styles.benefits}>
+        <h2>Nosso diferencial</h2>
+        <p className={styles.benefitsText}>
+          Diferenciais do João Vitor como seu Nutricionista:{" "}
+        </p>
+
+        <article className={styles.benefitsGroup}>
+          <div className={styles.benefitsGroupTitle}>
+            <p>01</p>
+            <h3>Diagnóstico 360º da Sua Vida:</h3>
+          </div>
+          <p className={styles.benefitsGroupText}>
+            Análise completa que vai além da nutrição, incluindo aspectos
+            emocionais, rotina diária, sono, estresse e atividades físicas. Isso
+            garante uma abordagem holística e personalizada, adaptada às
+            necessidades reais do paciente.
+          </p>
+        </article>
+
+        <article className={styles.benefitsGroup}>
+          <div className={styles.benefitsGroupTitle}>
+            <p>02</p>
+            <h3>Dieta Específica e 100% Personalizada:</h3>
+          </div>
+          <p className={styles.benefitsGroupText}>
+            Planos alimentares totalmente adaptados às necessidades,
+            preferências, estilo de vida e objetivos individuais. Nada de dietas
+            genéricas, apenas soluções sob medida.
+          </p>
+        </article>
+
+        <article className={styles.benefitsGroup}>
+          <div className={styles.benefitsGroupTitle}>
+            <p>03</p>
+            <h3>Suporte via WhatsApp:</h3>
+          </div>
+          <p className={styles.benefitsGroupText}>
+            Acesso direto para tirar dúvidas e receber orientações em tempo
+            real, sempre que precisar e nunca se sentir sozinho(a) na sua
+            jornada.
+          </p>
+        </article>
+
+        <article className={styles.benefitsGroup}>
+          <div className={styles.benefitsGroupTitle}>
+            <p>04</p>
+            <h3>Lista de Compras Personalizada e Prática:</h3>
+          </div>
+          <p className={styles.benefitsGroupText}>
+            Cada item cuidadosamente selecionado para atender à dieta
+            específica, facilitando a vida do paciente e garantindo que ele
+            tenha tudo o que precisa para seguir o plano alimentar sem
+            dificuldades.
+          </p>
+        </article>
+
+        <article className={styles.benefitsGroup}>
+          <div className={styles.benefitsGroupTitle}>
+            <p>05</p>
+            <h3>Planejamento de Refeições da Semana:</h3>
+          </div>
+          <p className={styles.benefitsGroupText}>
+            Organização completa de todas as refeições da semana, desde o café
+            da manhã até o jantar, adaptada ao gosto e às necessidades do
+            paciente. Sem improvisos, apenas refeições equilibradas e fáceis de
+            seguir.
+          </p>
+        </article>
+
+        <article className={styles.benefitsGroup}>
+          <div className={styles.benefitsGroupTitle}>
+            <p>06</p>
+            <h3>Acompanhamento:</h3>
+          </div>
+          <p className={styles.benefitsGroupText}>
+            Acima da média Monitoramento contínuo com ajustes na dieta e no
+            plano de ação, mesmo após a entrega inicial, garantindo que os
+            resultados sejam alcançados e sustentados ao longo do tempo.
+          </p>
+        </article>
       </section>
 
       <div className={styles.buttonGroup}>
