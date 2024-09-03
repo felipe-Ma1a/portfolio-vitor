e70c9ad00425db9a5daec6918263e5ba788b1f10
Felipe Maia <felipemaia850@gmail.com>
2024-09-03 12:03:25 -0300

    feat: change colors and texts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Button from "@/components/Button";
 import CardImage from "@/components/CardImage";
 import ListItem from "@/components/ListItem";
+import { FaCheckSquare } from "react-icons/fa";
 
 import cardImg1 from "public/images/flexvitor-9.jpg";
 import cardImg2 from "public/images/flexvitor-5.jpg";
@@ -16,7 +17,7 @@ export default function Home() {
     <main className={styles.container}>
       <section className={styles.hero}>
         <CardImage
-          src={cardImg1}
+          src={cardImg6}
           className={`${styles.cardImg} ${styles.imgHero}`}
         />
         <h1>Transforme sua vida com quem já esteve no seu lugar</h1>
@@ -25,71 +26,10 @@ export default function Home() {
         </div>
       </section>
 
-      <section className={`${styles.card} ${styles.cardMain}`}>
-        <h2 className={styles.cardSubtitle}>Quem sou eu?</h2>
-        <div className={styles.cardGroup}>
-          <p className={styles.cardInfo}>
-            Olá, meu nome é João Vitor, estou graduando nutrição. Minha jornada
-            até aqui não foi fácil. Eu sei o que é olhar no espelho e não se
-            reconhecer. Eu já estive muito acima do peso, já fiquei muito
-            abaixo, e, por um longo tempo, fui prisioneiro da compulsão
-            alimentar.
-          </p>
-          <CardImage
-            src={cardImg2}
-            className={`${styles.cardImg} ${styles.cardImgSmall}`}
-          />
-        </div>
-        <h3 className={styles.cardImgSmallSubtitle}>
-          Sim, já fui as duas pessoas!
-        </h3>
-      </section>
-
-      <section className={`${styles.card} ${styles.cardReverse}`}>
-        <CardImage src={cardImg3} className={styles.cardImg} />
-        <p className={styles.cardInfo}>
-          Mas foi através dessas experiências que encontrei minha verdadeira{" "}
-          <b>vocação</b>: ajudar pessoas como você a retomar o controle sobre a
-          própria vida. Decidi estudar <b>nutrição</b> porque sabia que, com o
-          conhecimento certo, poderia <b>transformar</b> minha realidade e a de
-          outros.
-        </p>
-      </section>
-
-      <section className={styles.card}>
-        <CardImage src={cardImg4} className={styles.cardImg} />
-        <p className={styles.cardInfo}>
-          E foi exatamente isso que aconteceu. Hoje, tenho as <b>ferramentas</b>
-          , o <b>conhecimento</b> e a <b>experiência</b> de quem já{" "}
-          <span>passou pelo que você está passando.</span>
-        </p>
-      </section>
-
-      <section className={styles.card}>
-        <p className={styles.cardInfo}>
-          Eu não estou aqui para te julgar ou te dizer que a <b>mudança</b> é
-          fácil. Estou aqui para te mostrar que é <b>possível</b>. Se você está
-          cansado de lutar sozinho, se quer finalmente ver{" "}
-          <b>resultados reais</b> e duradouros, eu estou aqui para{" "}
-          <b>guiar cada passo</b> da sua <b>transformação</b>.
-        </p>
-        <CardImage src={cardImg5} className={styles.cardImg} />
-      </section>
-
-      <section className={`${styles.card} ${styles.cardReverse}`}>
-        <p className={styles.cardInfo}>
-          Clique no link abaixo e venha conversar comigo. Juntos, podemos
-          construir o caminho para uma vida mais <b>saudável</b>,{" "}
-          <b>equilibrada</b> e <b>feliz</b>. Eu <b>acredito</b> em você, porque
-          eu já estive no seu lugar e sei que a <b>mudança é possível</b>.
-        </p>
-        <CardImage src={cardImg6} className={styles.cardImg} />
-      </section>
-
       <section className={styles.benefits}>
         <div className={styles.benefitsText}>
           <h2>Nosso diferencial</h2>
-          <p>Diferenciais do João Vitor como seu Nutricionista: </p>
+          <p>Diferenciais da nossa equipe: </p>
         </div>
 
         <article className={styles.benefitsGroup}>
@@ -177,19 +117,29 @@ export default function Home() {
         <ul>
           <ListItem text="Perca de gordura" />
           <ListItem text="Definição muscular" />
-          <ListItem text="Ganho de massa magra (Ganhar músculos corretamente)" />
+          <ListItem text="Ganho de massa magra" />
           <ListItem text="Suplementação (caso necessário)" />
-          <ListItem text="Ciclo menstrual" />
-          <ListItem
-            text="Alimentação específica para diferentes fases do ciclo menstrual,
+          <li className={`${styles.listItem} ${styles.listItemWoman}`}>
+            <FaCheckSquare size={16} color="#d0abf1" />
+            Ciclo menstrual
+          </li>
+          <li className={`${styles.listItem} ${styles.listItemWoman}`}>
+            Alimentação específica para diferentes fases do ciclo menstrual,
             ajudando a aliviar sintomas como inchaço, fadiga, e alterações de
-            humor."
-          />
-          <ListItem
-            text="Nutrição para Gravidez e Pós parto, Suporte nutricional para
-            mulheres grávidas e no pós-parto, visando a saúde da mãe e do bebê."
-          />
-          <ListItem text="Melhora nos quadros clínicos de saúde" />
+            humor.
+          </li>
+          <li className={`${styles.listItem} ${styles.listItemWoman}`}>
+            <FaCheckSquare size={16} color="#d0abf1" />
+            Nutrição para Gravidez e Pós parto
+          </li>
+          <li className={`${styles.listItem} ${styles.listItemWoman}`}>
+            Suporte nutricional para mulheres grávidas e no pós-parto, visando a
+            saúde da mãe e do bebê.
+          </li>
+          <li className={`${styles.listItem} ${styles.listItemWoman}`}>
+            <FaCheckSquare size={16} color="#d0abf1" />
+            Melhora nos quadros clínicos de saúde
+          </li>
         </ul>
       </section>
 
@@ -201,13 +151,91 @@ export default function Home() {
 
         <ul>
           <ListItem text="Perca de gordura" />
-          <ListItem text="Ganho de massa magra (músculos) corretamente" />
+          <ListItem text="Ganho de massa magra corretamente" />
           <ListItem text="Definição muscular" />
           <ListItem text="Suplementação (caso necessário)" />
-          <ListItem text="Melhora no desempenho de esportes" />
-          <ListItem text="Melhora no desempenho dos treinos de musculação" />
-          <ListItem text="Consulta para Pré e Pós-Treino: Orientação específica sobre alimentação antes e depois dos treinos, ajustando para diferentes tipos de atividade física" />
+          <li className={`${styles.listItem} ${styles.listItemMan}`}>
+            <FaCheckSquare size={16} color="#1e98d9" />
+            Melhora no desempenho de esportes
+          </li>
+          <li className={`${styles.listItem} ${styles.listItemMan}`}>
+            <FaCheckSquare size={16} color="#1e98d9" />
+            Melhora no desempenho dos treinos de musculação
+          </li>
+          <li className={`${styles.listItem} ${styles.listItemMan}`}>
+            <FaCheckSquare size={16} color="#1e98d9" />
+            Consulta para Pré e Pós-Treino
+          </li>
+          <li className={`${styles.listItem} ${styles.listItemMan}`}>
+            Orientação específica sobre alimentação antes e depois dos treinos,
+            ajustando para diferentes tipos de atividade física
+          </li>
         </ul>
+      </section>
+
+      <Button />
+
+      <section className={`${styles.card} ${styles.cardMain}`}>
+        <h2 className={styles.cardSubtitle}>Quem sou eu?</h2>
+        <div className={styles.cardGroup}>
+          <p className={styles.cardInfo}>
+            Olá, meu nome é João Vitor, faço parte de uma equipe de
+            profissionais da saúde dedicada a <b>transformar vidas</b>.
+            <br />
+            Minha jornada até aqui não foi fácil. <b>
+              Entendo profundamente
+            </b>{" "}
+            os desafios de quem luta contra a balança. Já estive em{" "}
+            <b>ambos os extremos</b> – muito acima e muito abaixo do peso, e por
+            um longo tempo, fui prisioneiro da <b>compulsão alimentar</b>.
+          </p>
+        </div>
+        <h3 className={styles.cardImgSmallSubtitle}>
+          <CardImage src={cardImg2} className={styles.cardImg} />
+          Sim, já fui as duas pessoas!
+        </h3>
+      </section>
+
+      <section className={`${styles.card} ${styles.cardReverse}`}>
+        <CardImage src={cardImg3} className={styles.cardImg} />
+        <p className={styles.cardInfo}>
+          Mas foi através dessas experiências que encontrei minha verdadeira{" "}
+          <b>vocação</b>: ajudar pessoas como você a retomar o controle sobre a{" "}
+          <b>própria vida</b>. Decidi estudar <b>nutrição</b> porque sabia que,
+          com o conhecimento certo, poderia <b>transformar</b> minha realidade e
+          a de outros.
+        </p>
+      </section>
+
+      <section className={styles.card}>
+        <CardImage src={cardImg4} className={styles.cardImg} />
+        <p className={styles.cardInfo}>
+          E foi exatamente isso que aconteceu. Hoje, tenho as <b>ferramentas</b>
+          , o <b>conhecimento</b> e a <b>experiência</b> de quem já{" "}
+          <span>passou pelo que você está passando.</span>
+        </p>
+      </section>
+
+      <section className={styles.card}>
+        <p className={styles.cardInfo}>
+          Eu não estou aqui para te julgar ou te dizer que a <b>mudança</b> é
+          fácil. Estou aqui para te mostrar que é <b>possível</b>. Se você está
+          cansado de lutar sozinho, se quer finalmente ver{" "}
+          <b>resultados reais</b> e <b>duradouros</b>, eu estou aqui para{" "}
+          <b>guiar cada passo</b> da sua <b>transformação</b>.
+        </p>
+        <CardImage src={cardImg5} className={styles.cardImg} />
+      </section>
+
+      <section className={`${styles.card} ${styles.cardReverse}`}>
+        <CardImage src={cardImg1} className={styles.cardImg} />
+        <p className={styles.cardInfo}>
+          Clique no link abaixo e venha conversar comigo. Juntos, podemos
+          construir o caminho para uma vida mais <b>saudável</b>,{" "}
+          <b>equilibrada</b> e <b>feliz</b>. Eu <b>acredito</b> em você, porque
+          eu <b>já estive no seu lugar</b> e sei que a <b>mudança é possível</b>
+          .
+        </p>
       </section>
 
       <div className={styles.buttonGroup}>
--- src/components/ListItem/index.tsx
+++ src/components/ListItem/index.tsx
@@ -2,9 +2,14 @@ import { FaCheckSquare } from "react-icons/fa";
 
 import styles from "./styles.module.css";
 
-const ListItem = ({ text }: { text: string }) => {
+type ListItemProps = {
+  text: string;
+  className?: string;
+};
+
+const ListItem = ({ text, className }: ListItemProps) => {
   return (
-    <li className={styles.listItem}>
+    <li className={`${styles.listItem} ${className}`}>
       <FaCheckSquare size={16} color="#2374c8" />
       {text}
     </li>
